4c30bb61347bff641474db73fcf29b6d82a18a91
NatalyaKya <natalya_kya@mail.ru>
2023-11-15 16:59:08 +1000

    fix:router

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,14 @@ const router = require('express').Router();
 const rateLimit = require('express-rate-limit');
 const routerUser = require('./users');
 const routerMovie = require('./movies');
-const auth = require('../middlewares/auth');
-const {
-  login,
-  createUser,
-  logout,
-} = require('../controllers/users');
-const { validationLogin, validationCreateUser } = require('../middlewares/validation');
+
 const NotFoundError = require('../errors/not-found-err');
 
 const limiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 100 });
+
 router.use(limiter);
-router.post('/signin', validationLogin, login);
-router.post('/signup', validationCreateUser, createUser);
-router.get('/signout', auth, logout);
-router.use('/users', auth, routerUser);
-router.use('/movies', auth, routerMovie);
+router.use(routerUser);
+router.use(routerMovie);
 router.all('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,24 @@ const routerUser = require('express').Router();
 const {
   returnCurrentUser,
   updateUser,
+  login,
+  createUser,
+  logout,
 } = require('../controllers/users');
 const {
   validationReturnCurrentUser,
   validationUpdateUser,
+  validationLogin,
+  validationCreateUser,
 } = require('../middlewares/validation');
+const auth = require('../middlewares/auth');
 
-routerUser.get('/me', validationReturnCurrentUser, returnCurrentUser);
-routerUser.patch('/me', validationUpdateUser, updateUser);
+routerUser.post('/signin', validationLogin, login);
+routerUser.post('/signup', validationCreateUser, createUser);
+routerUser.get('/signout', auth, logout);
+
+routerUser.use('/users', auth, routerUser);
+routerUser.get('users/me', validationReturnCurrentUser, auth, returnCurrentUser);
+routerUser.patch('users/me', validationUpdateUser, auth, updateUser);
 
 module.exports = routerUser;
